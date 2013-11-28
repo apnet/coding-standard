7588ad531e4c826860dd87a001caab1b92810735
Andrey F. Mindubaev <covex.mobile@gmail.com>
2013-11-28 23:12:09 +0400

    Fixed phpunit bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -9,5 +9,3 @@ if (!file_exists(__DIR__ . "/vendor/autoload.php")) {
 }
 
 require_once __DIR__ . "/vendor/autoload.php";
-
-require_once __DIR__ . "/vendor/squizlabs/php_codesniffer/tests/AllTests.php";
