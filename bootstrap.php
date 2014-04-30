dcaca1c5ba717b9bca05ad00f7c3d21a31bfb1fa
Andrey F. Mindubaev <covex.mobile@gmail.com>
2014-04-30 19:48:20 +0400

    exit() and die() functions should be avoided

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 
 if (!file_exists(__DIR__ . "/vendor/autoload.php")) {
-  die(
+  echo
     'You need to set up the project dependencies using the following commands:' . PHP_EOL .
     'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
-    'php composer.phar install' . PHP_EOL
-  );
+    'php composer.phar install' . PHP_EOL;
+} else {
+  require_once __DIR__ . "/vendor/autoload.php";
 }
-
-require_once __DIR__ . "/vendor/autoload.php";
