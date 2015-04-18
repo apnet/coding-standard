cd398e3bbf7d3e384f298cf3bfef8e656ef05c12
Andrey F. Mindubaev <covex.mobile@gmail.com>
2015-04-18 15:29:17 +0300

    Upgrade to squizlabs/php_codesniffer ~2.0

---
+++ src/Apnet/Tests/CodeSnifferTestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Apnet\Tests;
+
+/**
+ * CodeSniffer TestCase
+ */
+abstract class CodeSnifferTestCase extends \PHPUnit_Framework_TestCase
+{
+
+  /**
+   * Create PHP_CodeSniffer instance
+   *
+   * @param array $restrictions Restrictions
+   *
+   * @return \PHP_CodeSniffer
+   */
+  protected function getCodeSniffer(array $restrictions)
+  {
+    $codeSniffer = new \PHP_CodeSniffer();
+    $codeSniffer->cli->setCommandLineValues(
+      array("--report=summary")
+    );
+
+    $infoReporting = $codeSniffer->reporting->factory("summary");
+    /** @var \PHP_CodeSniffer_Reports_Info $infoReporting */
+    $infoReporting->recordErrors = true;
+
+    $codeSniffer->process(array(), __DIR__ . "/..", $restrictions);
+    $codeSniffer->setIgnorePatterns(array());
+
+    return $codeSniffer;
+  }
+}
